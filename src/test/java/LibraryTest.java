69908bea489180effa621078a07e628bda278b3e
Andrew Crossan <andrewcrossanit@gmail.com>
2021-11-17 00:56:09 +0000

    initial commit

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void setUp(){
+        library = new Library();
+        book1 = new Book("Willy Wonka", "Roald Dahl", "Childrens' Books");
+        book2 = new Book("Foundation", "Isaac Asimov", "Sci-Fi");
+    }
+
+    @Test
+    public void addBookToLibraryCollection(){
+        library.addBook(book1);
+        assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void checkIfLibraryIsFull(){
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        assertEquals(5, library.bookCount());
+    }
+
+}
